0c97085ed83581d6b7fd6300934b59acf9b19a45
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2023-11-29 15:26:56 +0100

    completed

---
+++ js/task-1.js
@@ -0,0 +1,40 @@
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює,
+// чи помістяться всі товари в контейнер при пакуванні.
+
+// Функція оголошує два параметри:
+
+// products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість
+// цих товарів.Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе
+// може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер.
+// Тобто порахувати загальну кількість товарів в об’єкті products і повернути true,
+//   якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності
+// її роботи.У консоль будуть виведені результати її викликів.
+
+function isEnoughCapacity(products, containerSize) {
+  // const sum = Object.values(products).reduce()
+
+  let sum = 0;
+  for (const product in products) {
+    sum += products[product];
+  }
+
+  // if (sum >= containerSize) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+
+  return sum >= containerSize ? false : true;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
